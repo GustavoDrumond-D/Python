0efdde9d4962ac93b2658a9518d92e6c87e32c62
Gustavo Drumond <gdrumondduarte@gmail.com>
2025-02-25 20:09:28 -0300

    Atualizando develop

---
+++ learning/Sabor Express/app.py
@@ -0,0 +1,24 @@
+# Comando que imprime no terminal
+print("""
+╭━━━╮╱╱╭╮╱╱╱╱╱╱╱╭━━━╮
+┃╭━╮┃╱╱┃┃╱╱╱╱╱╱╱┃╭━━╯
+┃╰━━┳━━┫╰━┳━━┳━╮┃╰━━┳╮╭┳━━┳━┳━━┳━━┳━━╮
+╰━━╮┃╭╮┃╭╮┃╭╮┃╭╯┃╭━━┻╋╋┫╭╮┃╭┫┃━┫━━┫━━┫
+┃╰━╯┃╭╮┃╰╯┃╰╯┃┃╱┃╰━━┳╋╋┫╰╯┃┃┃┃━╋━━┣━━┃
+╰━━━┻╯╰┻━━┻━━┻╯╱╰━━━┻╯╰┫╭━┻╯╰━━┻━━┻━━╯
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃
+╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯
+      """);
+
+
+print('1. Cadastrar restaurante');
+print('2. Listar restaurantes');
+print('3. Ativar restaurante');
+print('4. sair');
+
+# Comando que pega uma informação do usuário através do terminal
+opcao = input('Escolha uma opção:')
+
+# print('Voce escolheu a opcao: ', opcao);
+# f-string é uma forma de concatenar strings
+print(f'Voce escolheu a opcao: {opcao}')
