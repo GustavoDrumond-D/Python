4596cbf1267c075f9826becaf7c720d82b11174a
Gustavo Drumond <gdrumondduarte@gmail.com>
2025-02-25 21:12:52 -0300

    Atualizando develop

--- learning/Sabor Express/app.py
+++ learning/Sabor Express/app.py
@@ -1,5 +1,10 @@
-# Comando que imprime no terminal
-print("""
+import os
+
+def finalizar_app():
+    os.system('cls');
+    print('Finalizando...\n');
+def exibir_nome_do_programa():
+    print("""
 ╭━━━╮╱╱╭╮╱╱╱╱╱╱╱╭━━━╮
 ┃╭━╮┃╱╱┃┃╱╱╱╱╱╱╱┃╭━━╯
 ┃╰━━┳━━┫╰━┳━━┳━╮┃╰━━┳╮╭┳━━┳━┳━━┳━━┳━━╮
@@ -9,16 +14,30 @@ print("""
 ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃
 ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯
       """);
+def exibir_opcoes():
+    print('1. Cadastrar restaurante');
+    print('2. Listar restaurantes');
+    print('3. Ativar restaurante');
+    print('4. sair');
+def opcoes():
+    opcao = int(input('Escolha uma opção:'))
+    # opcao = int(opcao)
+    if opcao == 1:
+        print('opção: cadastrar restaurante');
+    elif opcao == 2:
+        print('opção: listar restaurantes');
+    elif opcao == 3:
+        print('opção: ativar restaurante');
+    elif opcao == 4:
+        print('opção: sair');
+        finalizar_app();
+    else:
+        print('Opção inválida');
 
+def main():
+    exibir_nome_do_programa();
+    exibir_opcoes();
+    opcoes();
 
-print('1. Cadastrar restaurante');
-print('2. Listar restaurantes');
-print('3. Ativar restaurante');
-print('4. sair');
-
-# Comando que pega uma informação do usuário através do terminal
-opcao = input('Escolha uma opção:')
-
-# print('Voce escolheu a opcao: ', opcao);
-# f-string é uma forma de concatenar strings
-print(f'Voce escolheu a opcao: {opcao}')
+if __name__ == '__main__':
+    main()
