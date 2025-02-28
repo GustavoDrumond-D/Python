856f072b11fb04da659d7e330440d3322d71bbb1
Gustavo Drumond <gdrumondduarte@gmail.com>
2025-02-27 21:17:36 -0300

    Atualizando develop

--- learning/Sabor Express/app.py
+++ learning/Sabor Express/app.py
@@ -1,4 +1,6 @@
 import os
+restaurantes = [];
+
 
 def finalizar_app():
     os.system('cls');
@@ -18,23 +20,88 @@ def exibir_opcoes():
     print('1. Cadastrar restaurante');
     print('2. Listar restaurantes');
     print('3. Ativar restaurante');
-    print('4. sair');
+    print('4. Deletar restaurante');
+    print('5. sair');
+def opcao_invalida():
+    print('Opção inválida!');
+    input('Pressione qualquer tecla para continuar...');
+    main();
+
+def cadastrar_restaurante():
+    os.system('cls');
+    try:
+        nome_restaurante = input('Forneça o nome do restaurante:\n');
+        if nome_restaurante == '':
+            print('Nome invalido');
+            main();
+        else:
+            # .append() adiciona um item ao final da lista
+            restaurantes.append(nome_restaurante);
+            print(f'Restaurante {nome_restaurante} cadastrado com sucesso!')
+            input('Pressione qualquer tecla para continuar...');
+            main();
+    except:
+        print('Erro ao cadastrar restaurante');
+        input('Pressione qualquer tecla para continuar...');
+        main();
+
+def listar_restuarntes():
+    os.system('cls');
+    print('Listando restaurantes...');
+    
+    for restaurante in restaurantes:
+        # .index retorna o index do item
+        print(f'{restaurantes.index(restaurante) + 1}. {restaurante}');
+    
+    input('Pressione qualquer tecla para continuar...');
+    main();
+
+
+# def ativar_restaurante():
+
+# def deletar_restaurante():
+
 def opcoes():
-    opcao = int(input('Escolha uma opção:'))
-    # opcao = int(opcao)
-    if opcao == 1:
-        print('opção: cadastrar restaurante');
-    elif opcao == 2:
-        print('opção: listar restaurantes');
-    elif opcao == 3:
-        print('opção: ativar restaurante');
-    elif opcao == 4:
-        print('opção: sair');
-        finalizar_app();
-    else:
-        print('Opção inválida');
+    try:
+        opcao = int(input('Escolha uma opção:'))
+        # # opcao = int(opcao)
+        # if opcao == 1:
+        #     print('opção: cadastrar restaurante');
+        # elif opcao == 2:
+        #     print('opção: listar restaurantes');
+        # elif opcao == 3:
+        #     print('opção: ativar restaurante');
+        # elif opcao == 4:
+        #     print('opção: sair');
+        #     finalizar_app();
+        # else:
+        #     print('Opção inválida');
+
+        # match parece com o switch case, mas em python não precisa de chaves
+        match opcao:
+            case 1:
+                print('opção: cadastrar restaurante');
+                cadastrar_restaurante();
+            case 2:
+                print('opção: listar restaurantes');
+                listar_restuarntes();
+            case 3:
+                print('opção: ativar restaurante');
+                # ativar_restaurante();
+            case 4:
+                print('opção: deletar restaurante');
+                # deletar_restaurante();
+            case 5:
+                print('opção: sair');
+                finalizar_app();
+            case _:
+                opcao_invalida();
+    except:
+        opcao_invalida();
+        
 
 def main():
+    os.system('cls');
     exibir_nome_do_programa();
     exibir_opcoes();
     opcoes();
---
+++ learning/practice/pratica_02.py
@@ -0,0 +1,72 @@
+import os
+
+# 1 - Solicite ao usuário que insira um número e, em seguida, use uma estrutura if else para determinar se o número é par ou ímpar.
+
+numero = int(input('Insira um numero: '));
+
+# o operdador % retorna o resto da divisão, neste caso, se o resto for 0, retorna par, caso contrario, retorna impar
+if numero % 2 == 0:
+    print(f'O numero {numero} é par.');
+else:
+    print(f'O numero {numero} é impar.');
+
+
+
+
+# 2 - Pergunte ao usuário sua idade e, com base nisso, use uma estrutura if elif else para classificar a idade em categorias de acordo com as seguintes condições:
+
+# Criança: 0 a 12 anos;
+# Adolescente: 13 a 18 anos;
+# Adulto: acima de 18 anos.
+
+idade = int(input('Insira sua idade: '));
+
+if idade <= 12:
+    print('criança');
+elif idade >= 13 and idade <= 18:
+    print('adolescente');
+elif idade > 18:
+    print('adulto');
+else:
+    print('dados invalidos')
+
+
+
+# 3 - Solicite um nome de usuário e uma senha e use uma estrutura if else para verificar se o nome de usuário e a senha fornecidos correspondem aos valores esperados determinados por você.
+
+nome = input('Insira seu nome:');
+senha = input('Insira sua senha:');
+
+nome_esperado = 'Gustavo';
+senha_esperada = '1234';
+
+if nome == nome_esperado and senha == senha_esperada:
+    print('Acesso permitido');
+else:
+    print('Acesso negado');
+
+
+
+# 4 - Solicite ao usuário as coordenadas (x, y) de um ponto qualquer e utilize uma estrutura if elif else para determinar em qual quadrante do plano cartesiano o ponto se encontra de acordo com as seguintes condições:
+
+# Primeiro Quadrante: os valores de x e y devem ser maiores que zero;
+# Segundo Quadrante: o valor de x é menor que zero e o valor de y é maior que zero;
+# Terceiro Quadrante: os valores de x e y devem ser menores que zero;
+# Quarto Quadrante: o valor de x é maior que zero e o valor de y é menor que zero;
+# Caso contrário: o ponto está localizado no eixo ou origem.
+
+x = int(input('Insira o valor de x:'));
+y = int(input('Insira o valor de y:'));
+
+if x > 0 and y > 0:
+    print('Primeiro Quadrante');
+elif x < 0 and y > 0:
+    print('Segundo Quadrante');
+elif x < 0 and y < 0:
+    print('Terceiro Quadrante');
+elif x > 0 and y < 0:
+    print('Quarto Quadrante');
+elif x == 0 and y == 0:
+    print('Origem');
+else:
+    print('Eixo ou origem');
