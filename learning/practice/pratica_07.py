e53ca93b0bcacd3a62d1a28d15ed0c2aad4c058f
Gustavo Drumond <gdrumondduarte@gmail.com>
2025-03-08 11:07:56 -0300

    atualizando develop

--- learning/practice/pratica_07.py
+++ learning/practice/pratica_07.py
@@ -12,55 +12,63 @@
 
 # 7.Crie um método de classe para a conta ClienteBanco.
 
-# class ContaBancaria:
-#     def __init__(self, titular, saldo):
-#         self.titular = titular
-#         self.saldo = saldo
-#         self._ativo = False
-
-#     def __str__(self):
-#         return f'Titular da conta: {self.titular} | Saldo: {self.saldo}'
-    
-#     @classmethod
-#     def ativar_conta(cls, conta):
-#         cls._ativo = True
-
-    
-# conta_01 = ContaBancaria('João', 1020)
-# conta_02 = ContaBancaria('Maria', 500)
-
-# print(conta_01)
-# print(conta_02)
-
-# conta_03 = ContaBancaria('Pedro', 1000)
-# print(f'status da conta: {conta_01._ativo}')
-# ContaBancaria.ativar_conta(conta_03)
-# print(f'status da conta: {conta_01._ativo}')
-
-# 1) Crie uma classe chamada `ContaBancaria` com um construtor que aceita os parâmetros titular e saldo. Inicie o atributo ativo como False por padrão.
 class ContaBancaria:
     def __init__(self, titular, saldo):
         self.titular = titular
         self.saldo = saldo
-        self._ativo = False  # Atributo protegido
+        self._ativo = False
 
-# 2) Na classe ContaBancaria, adicione um método especial `__str__` que retorna uma mensagem formatada com o titular e o saldo da conta. Crie duas instâncias da classe e imprima essas instâncias.
     def __str__(self):
-        return f"Conta de {self.titular} - Saldo: R${self.saldo}"
+        return f'Titular da conta: {self.titular} | Saldo: {self.saldo}'
     
+    @classmethod
     def ativar_conta(cls, conta):
-        conta._ativo = True
+        cls._ativo = True
 
-conta1 = ContaBancaria("João", 1000)
-conta2 = ContaBancaria("Maria", 500)
-
-print(conta1)
-print(conta2)
-
-# 3) Adicione um método de classe chamado `ativar_conta` à classe `ContaBancaria` que define o atributo ativo como `True`. Crie uma instância da classe, chame o método de classe e imprima o valor de ativo.
     
+conta_01 = ContaBancaria('João', 1020)
+conta_02 = ContaBancaria('Maria', 500)
 
-conta3 = ContaBancaria("Carlos", 200)
-print(f"Antes de ativar: Conta ativa? {conta3._ativo}")
-ContaBancaria.ativar_conta(conta3)
-print(f"Depois de ativar: Conta ativa? {conta3._ativo}")
+print(conta_01)
+print(conta_02)
+
+conta_03 = ContaBancaria('Pedro', 1000)
+print(f'status da conta: {conta_01._ativo}')
+ContaBancaria.ativar_conta(conta_03)
+print(f'status da conta: {conta_01._ativo}')
+
+class ContaBancariaPythonica:
+    def __init__ (self, titular, saldo):
+        self._titular = titular;
+        self._saldo = saldo;
+        self._ativo = False;
+
+    @property
+    def titular(self):
+        return self._titular
+    
+    def saldo(self):
+        return self._saldo
+    
+    @property
+    def ativo(self):
+        return self._ativo
+    
+conta_04 = ContaBancariaPythonica('João', 1020)
+print(f'Titular: {conta_04.titular} | Saldo: {conta_04.saldo()}');
+
+class ClienteBanco:
+    def __init__ (self, id, titular, saldo, telefone, email):
+        self._id = id;
+        self._titular = titular;
+        self._saldo = saldo;
+        self._telefone = telefone;
+        self._email = email;
+
+    @classmethod
+    def criar_conta(cls, titular, saldo):
+        conta = ContaBancariaPythonica(titular, saldo)
+        return conta
+    
+conta_cliente = ClienteBanco.criar_conta('João', 1020)
+print(f'Titular: {conta_cliente.titular} | Saldo: {conta_cliente.saldo()}');
