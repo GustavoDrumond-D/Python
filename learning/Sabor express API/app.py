e319c6bf88ed499c751cc4e286febd462c508d68
Gustavo Drumond <gdrumondduarte@gmail.com>
2025-03-10 21:23:52 -0300

    atualizando develop

--- learning/Sabor express API/app.py
+++ learning/Sabor express API/app.py
@@ -6,7 +6,8 @@ restaurante_praca = Restaurante('praça', 'Gourmet')
 bebida_suco = Bebida('suco melancia', 5.00, 'melancia', 'grande')
 prato_carne = Prato('carne de porco', 20.00, 'carne de porco')
 
-
+restaurante_praca.adicionar_bebida_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_prato_no_cardapio(prato_carne)
 
 def main():
     print(bebida_suco)
---
+++ projetins/app_banco.py
@@ -0,0 +1,129 @@
+import os;
+import getpass;
+import random;
+
+
+class Banco:
+    contas = []
+    def __init__ (self, id, titular, senha):
+        self._id = id;
+        self._titular = titular;
+        self._senha = senha;
+        self._saldo = 0;
+
+    @classmethod
+    def criar_conta(cls):
+        try:
+            print('Opção: Criar Conta');
+            print();
+        
+            nome_titular = input('Insira o nome do titular: ');
+            if not nome_titular:
+                raise Exception('O nome do titular nao pode ser vazio');
+    
+            senha_titular = getpass.getpass('Insira a senha: ');
+            if not senha_titular:
+                raise Exception('A senha nao pode ser vazia');
+    
+            for conta in cls.contas:
+                if conta._titular == nome_titular:
+                    raise Exception('Ja existe uma conta com esse titular');
+
+            gerar_id = random.randint(1000, 9999);
+            nova_conta = Banco(gerar_id, nome_titular, senha_titular)
+            cls.contas.append(nova_conta)
+
+            print('Conta criada com sucesso!')
+            print(f'ID da conta: {nova_conta._id}')
+            print(f'Titular: {nova_conta._titular}')
+            voltar_ao_menu()        
+
+
+        except Exception as e:
+            print('Erro ao criar conta');
+            print(f'Erro: {e}');
+            voltar_ao_menu();
+
+    @staticmethod
+    def entrar_conta():
+        print('Opção: Entrar na Conta');
+        print();
+    
+        try:
+            id_conta = int(input('Insira o ID da conta: '));
+            if not id_conta:
+                raise Exception('O ID da conta nao pode ser vazio');
+
+            senha_conta = getpass.getpass('Insira a senha: ');
+            if not senha_conta:
+                raise Exception('A senha nao pode ser vazia');
+            
+            if id_conta not in [conta._id for conta in Banco.contas]:
+                raise Exception('Nao existe uma conta com esse ID');
+
+            for conta in Banco.contas:
+                if conta._id == id_conta and conta._senha == senha_conta:
+                    print('Entrou na conta com sucesso!');
+                    print(f'ID da conta: {conta._id}');
+                    print(f'Titular: {conta._titular}');
+                    print(f'Saldo: {conta._saldo}');
+                    voltar_ao_menu();
+                 
+        except Exception as e:
+            print('Erro ao entrar na conta');
+            print(f'Erro: {e}');
+            voltar_ao_menu();
+            
+
+    @staticmethod
+    def listar_contas():
+        print('Opção: Listar contas');
+        print();
+        
+        for conta in Banco.contas:
+            print(f'ID: {conta._id}');
+            print(f'Titular: {conta._titular}');
+            print(f'Saldo: {conta._saldo}');
+            print();
+
+        voltar_ao_menu();
+
+def iniciar_sistema():
+    os.system('cls');
+    print('Bem vindo ao Banco XPTO!');
+    print();
+    print('''Esolha uma opção: \n1 - Criar Conta \n2 - Entrar na Conta \n3 - listar contas \n4 - Sair''');
+def voltar_ao_menu():
+    input('Pressione qualquer tecla para continuar... ');
+    main();
+def opcao_invalida():
+    print('Opção inválida!');
+
+
+
+def opcoes():
+    try:
+        opcao = int(input('Escolha uma opção: '));
+        match opcao:
+            case 1:
+                Banco.criar_conta();
+            case 2:
+                Banco.entrar_conta();
+            case 3:
+                Banco.listar_contas();
+            case 4:
+                pass
+            case _:
+                opcao_invalida();
+                voltar_ao_menu();
+    except Exception as e:
+        print(f'Erro: {e}');
+        voltar_ao_menu();
+
+def main():
+    os.system('cls');
+    iniciar_sistema();
+    opcoes();
+    
+if __name__ == '__main__':
+    main();
